1bec552f5035287c863d781e4a79800d9ba17eb5
Ilias Papanikolaou <eliaspapanikolaou@hotmail.com>
2023-10-22 13:20:22 +0300

    httpClient POST request

--- src/main/java/com/ipap/client/CountryHttpClient.java
+++ src/main/java/com/ipap/client/CountryHttpClient.java
@@ -1,18 +1,28 @@
 package com.ipap.client;
 
+import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ipap.springsoap.gen.Country;
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.EntityTemplate;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
 
 @Component
 public class CountryHttpClient {
@@ -25,9 +35,9 @@ public class CountryHttpClient {
         this.httpClient = httpClient;
     }
 
-    public Country getCountry(String name) throws IOException {
-        HttpGet request = new HttpGet("http://localhost:8080/api/countries/" + name);
-        CloseableHttpResponse response = httpClient.execute(request);
+    public Country getCountryGetMethod(String name) throws IOException {
+        HttpGet getRequest = new HttpGet("http://localhost:8080/api/countries/" + name);
+        CloseableHttpResponse response = httpClient.execute(getRequest);
 
         HttpEntity entity = response.getEntity();
 
@@ -39,5 +49,36 @@ public class CountryHttpClient {
         return mapper.readValue(EntityUtils.toString(entity), new TypeReference<>() { });
     }
 
+    public Country getCountryPostMethod(String name) throws IOException {
+        // Create Post request
+        JsonFactory jsonFactory = new JsonFactory();
+        ObjectMapper objectMapper = new ObjectMapper(jsonFactory);
 
+        HttpPost postRequest = new HttpPost("http://localhost:8080/api/countries");
+        NameValuePair requestData = new BasicNameValuePair("name", name);
+
+        EntityTemplate requestEntity = new EntityTemplate(outputStream -> {
+            objectMapper.writeValue(outputStream, requestData);
+            outputStream.flush();
+        });
+
+        requestEntity.setContentType(ContentType.APPLICATION_JSON.toString());
+        postRequest.setEntity(requestEntity);
+
+        JsonNode responseData = httpClient.execute(postRequest, response -> {
+            if (response.getStatusLine().getStatusCode() >= 300) {
+                throw new ClientProtocolException(Objects.toString(response.getStatusLine()));
+            }
+            final HttpEntity responseEntity = response.getEntity();
+            if (responseEntity == null) {
+                return null;
+            }
+            try (InputStream inputStream = responseEntity.getContent()) {
+                return objectMapper.readTree(inputStream);
+            }
+        });
+
+        log.info(responseData.toString());
+        return objectMapper.readValue(responseData.toString(), Country.class);
+    }
 }
--- src/main/java/com/ipap/controller/CountryController.java
+++ src/main/java/com/ipap/controller/CountryController.java
@@ -1,18 +1,21 @@
 package com.ipap.controller;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ipap.client.CountryHttpClient;
 import com.ipap.repository.CountryRepository;
 import com.ipap.springsoap.gen.Country;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class CountryController {
+
+    private static final Logger log = LoggerFactory.getLogger(CountryHttpClient.class.getName());
 
     private final CountryHttpClient countryHttpClient;
     private final CountryRepository countryRepository;
@@ -22,13 +25,44 @@ public class CountryController {
         this.countryRepository = countryRepository;
     }
 
+    // Called internally by HttpClient
     @GetMapping("/countries/{name}")
     public Country getCountryByName(@PathVariable String name) throws IOException {
         return countryRepository.findCountry(name);
     }
 
-    @GetMapping("/countries/invoke/{name}")
+    // Called internally by HttpClient
+    @PostMapping("/countries")
+    public Country postCountryByName(@RequestBody String json) throws IOException {
+        // Get value from JSON using Jackson
+        final JsonNode node = new ObjectMapper().readTree(json);
+        if (node.has("contentType")) {
+            log.debug("contentType: " + node.get("contentType"));
+        }
+        if (node.has("name")) {
+            log.debug("name: " + node.get("name"));
+        }
+        return countryRepository.findCountry(node.get("value").textValue());
+    }
+
+    // Call via Postman to invoke GET flow
+    @GetMapping("/countries/test/{name}")
     public Country invokeGetCountryByNameApi(@PathVariable String name) throws IOException {
-        return countryHttpClient.getCountry(name);
+        return countryHttpClient.getCountryGetMethod(name);
+    }
+
+    // Call via Postman to invoke POST flow
+    @PostMapping("/countries/test")
+    public Country invokePostCountryByNameApi(@RequestBody String json) throws IOException {
+        // Get value from JSON using Jackson
+        final JsonNode node = new ObjectMapper().readTree(json);
+        if (node.has("contentType")) {
+            log.debug("contentType: " + node.get("contentType"));
+        }
+        if (node.has("name")) {
+            log.debug("name: " + node.get("name"));
+        }
+        String name = node.get("name").textValue();
+        return countryHttpClient.getCountryPostMethod(name);
     }
 }
